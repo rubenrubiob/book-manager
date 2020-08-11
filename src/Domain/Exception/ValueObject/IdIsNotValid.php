417d40e63bf634d126e6e6ec3db6b573d1c07eea
Rubén Rubio Barrera <ruben.rubio.barrera@gmail.com>
2020-08-11 16:07:54 +0200

    Añadir Id ValueObject

---
+++ src/Domain/Exception/ValueObject/IdIsNotValid.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Exception\ValueObject;
+
+use Exception;
+use function Safe\sprintf;
+
+final class IdIsNotValid extends Exception
+{
+    public static function withFormat(string $id): self
+    {
+        return new self(
+            sprintf(
+                'Id "%s" does not have a valid format',
+                $id
+            )
+        );
+    }
+}

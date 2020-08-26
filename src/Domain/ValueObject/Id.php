0767604ee414aabe70d043f5266c60d87c5f55c5
Rubén Rubio Barrera <ruben.rubio.barrera@gmail.com>
2020-08-26 17:52:46 +0200

    Añadir Id ValueObject

---
+++ src/Domain/ValueObject/Id.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\ValueObject;
+
+use Domain\Exception\ValueObject\IdIsNotValid;
+use InvalidArgumentException;
+use Ramsey\Uuid\Uuid;
+use Webmozart\Assert\Assert;
+
+/**
+ * @psalm-immutable
+ */
+final class Id
+{
+    private string $id;
+
+    private function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public static function generate(): self
+    {
+        return new self(
+            Uuid::uuid4()->toString()
+        );
+    }
+
+    /**
+     * @throws IdIsNotValid
+     */
+    public static function fromString(string $id): self
+    {
+        self::validate($id);
+
+        return new self($id);
+    }
+
+    public function asString(): string
+    {
+        return $this->id;
+    }
+
+    public function equalsTo(Id $anotherId): bool
+    {
+        return $this->id === $anotherId->id;
+    }
+
+    /**
+     * @throws IdIsNotValid
+     */
+    private static function validate(string $id): void
+    {
+        try {
+            Assert::uuid($id);
+        } catch (InvalidArgumentException $e) {
+            throw IdIsNotValid::withFormat($id);
+        }
+    }
+}
